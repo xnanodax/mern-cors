9743e5a051598040b9d87ae7ba0b27b32b77996f
Cynthia Ma <cynthia.ma.emily@gmail.com>
2019-01-13 20:12:46 -0800

    Change api

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const axios = require("axios");
 
 const app = express();
 
-const url = `https://jobs.github.com/positions.json?description=python&full_time=true&location=sf`;
+// const url = `https://jobs.github.com/positions.json?description=python&full_time=true&location=sf`;
+const url = `https://api.exchangeratesapi.io/latest`;
+
+// middleware
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers", 
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.get("/", (req, res) => {
   const asyncCall = axios({
@@ -14,8 +25,8 @@ app.get("/", (req, res) => {
 
   asyncCall.then(
     response => { 
-      console.log(response.data[0]);
-      res.send(`${response.data.map(el => JSON.stringify(el))}`);
+      // console.log(response.data);
+      res.send(`${JSON.stringify(response.data)}`);
     }, 
     rej => res.send(`Error: \n ${rej.JSON}`));
   
