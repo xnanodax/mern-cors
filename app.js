61719bfa77f2b41f533980577c1958b9e8a7e683
Cynthia Ma <cynthia.ma.emily@gmail.com>
2019-01-13 17:36:04 -0800

    Add axios and CORS request

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const axios = require("axios");
+
 
 const app = express();
 
+const url = `https://jobs.github.com/positions.json?description=python&full_time=true&location=sf`;
+
 app.get("/", (req, res) => {
-  res.send("Hoasdfasd World");
+  const asyncCall = axios({
+    url,
+    method: 'get',
+  });
+
+  asyncCall.then(
+    response => { 
+      console.log(response.data[0]);
+      res.send(`${response.data.map(el => JSON.stringify(el))}`);
+    }, 
+    rej => res.send(`Error: \n ${rej.JSON}`));
+  
 });
 
 const PORT = process.env.PORT || 5000;
